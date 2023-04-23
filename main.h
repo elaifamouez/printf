04926bdabae1753628fb6a81d02a01ed0ea9e9c1
elaifa <mouezelaifa@gmail.com>
2023-04-23 11:26:34 -0700

    Task 1. Education is when you read the fine print. Experience is what you get if you don"t

--- main.h
+++ main.h
@@ -37,6 +37,5 @@ int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
 int print_int(va_list l, flags_t *f);
-int count_digit(int i);
 void print_number(int n);
 #endif
--- print_int.c
+++ print_int.c
@@ -8,9 +8,18 @@
  */
 int print_int(va_list l, flags_t *f)
 {
-int n = va_arg(l, int);
-int res = count_digit(n);
-
+int n = va_arg(l, int), res;
+unsigned int d = 0, u;
+if (i < 0)
+u = i * -1;
+else
+u = i;
+while (u != 0)
+{
+u /= 10;
+d++;
+}
+res = d;
 if (f->space == 1 && f->plus == 0 && n >= 0)
 res += _putchar(' ');
 if (f->plus == 1 && n >= 0)
